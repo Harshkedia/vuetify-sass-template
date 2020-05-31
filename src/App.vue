0b803ff6bd04b92aa001f2d8a5b9b0fd2a73733e
Harsh Kedia <harshkedi@gmail.com>
2020-05-31 14:40:29 -0700

    updated styling

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,39 @@
   <v-app>
     <v-content>
       <div class="app-grid">
-        <v-tabs v-model="activeTab" centered fixed-tabs color="#002855">
-          <v-tab>
-            Option A
-          </v-tab>
-          <v-tab>
-            Option B
-          </v-tab>
-          <v-tab-item class="tab-text">
-            <div class="left-row">
-              <program-sliders :option="optionA" />
-              <cost-sliders :option="optionA" />
-              <total-costs :option="optionA" />
-            </div>
-          </v-tab-item>
-          <v-tab-item class="tab-text">
-            <div class="left-row">
-              <program-sliders :option="optionB" />
-              <cost-sliders :option="optionB" />
-              <total-costs :option="optionB" />
-            </div>
-          </v-tab-item>
-        </v-tabs>
-        <div class="right-row">
-          <model-viewer v-if="optionAActive" :option="optionA" />
-          <model-viewer v-if="!optionAActive" :option="optionB" />
-          <cost-chart />
-          <div><br /><br /><br /></div>
-          <div><br /><br /><br /></div>
-        </div>
+        <v-card outlined>
+          <v-tabs v-model="activeTab" centered fixed-tabs color="#002855">
+            <v-tab>
+              Option A
+            </v-tab>
+            <v-tab>
+              Option B
+            </v-tab>
+            <v-tab-item class="tab-text">
+              <div class="left-row">
+                <program-sliders :option="optionA" />
+                <cost-sliders :option="optionA" />
+                <total-costs :option="optionA" />
+              </div>
+            </v-tab-item>
+            <v-tab-item class="tab-text">
+              <div class="left-row">
+                <program-sliders :option="optionB" />
+                <cost-sliders :option="optionB" />
+                <total-costs :option="optionB" />
+              </div>
+            </v-tab-item>
+          </v-tabs>
+        </v-card>
+        <v-card outlined>
+          <div class="right-row">
+            <model-viewer v-if="optionAActive" :option="optionA" />
+            <model-viewer v-if="!optionAActive" :option="optionB" />
+            <cost-chart />
+            <div><br /><br /><br /></div>
+            <div><br /><br /><br /></div>
+          </div>
+        </v-card>
       </div>
       <Footer />
     </v-content>
